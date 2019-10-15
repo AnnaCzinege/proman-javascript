fd087b212ce6081599fcf109b13e96ea84d2c70a
rasgacode <rongybaba12@gmail.com>
2019-10-15 15:06:26 +0200

    +show tables

--- data_handler.py
+++ data_handler.py
@@ -17,41 +17,31 @@ def normalize_output_multiple_rows(normalize_me):  # Creates dictionaries in a l
             dump_dictionary[key] = value
         normalized_output.append(dump_dictionary)
     return normalized_output
-'''
-def get_card_status(status_id):
-    """
-    Find the first status matching the given id
-    :param status_id:
-    :return: str
-    """
-    statuses = persistence.get_statuses()
-    return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
-
-
-def get_boards():
-    """
-    Gather all boards
-    :return:
-    """
-    return persistence.get_boards(force=True)
-
-
-def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards '''
 
 
 @database_common.connection_handler
 def get_boards(cursor):
     cursor.execute("""
-                    SELECT * FROM boards
+                    SELECT * FROM boards;
                     """)
-    return normalize_output_multiple_rows(cursor.fetchall())
+    boards = normalize_output_multiple_rows(cursor.fetchall())
+
+    cursor.execute("""
+                    SELECT * FROM cards;                    
+                    """)
+    cards = normalize_output_multiple_rows(cursor.fetchall())
+
+    cursor.execute("""
+                    SELECT * FROM statuses;                    
+                    """)
+    statuses = normalize_output_multiple_rows(cursor.fetchall())
+
+    return {
+        "boards": boards,
+        "cards": cards,
+        "statuses": statuses
+        }
+
+
 
 
--- static/js/dom.js
+++ static/js/dom.js
@@ -8,7 +8,7 @@ export let dom = {
     loadBoards: function () {
         // retrieves boards and makes showBoards called
         dataHandler.getBoards(function(boards){
-            dom.showBoards(boards);
+            dom.testFunction(boards);
         });
     },
     showBoards: function (boards) {
@@ -17,10 +17,8 @@ export let dom = {
 
         let boardList = '';
 
-        for(let board of boards){
-            boardList += `
-                <li>${board.title}</li>
-            `;
+        for(let board of boards.boards){
+            boardList += `<li>${board.title}</li>`;
         }
 
         const outerHtml = `
@@ -39,5 +37,73 @@ export let dom = {
         // shows the cards of a board
         // it adds necessary event listeners also
     },
-    // here comes more features
+    testFunction: function (boards) {
+        let boardContainer = document.createElement("div");
+        for (let board of boards.boards) {
+
+
+            let boardSection = document.createElement("section");
+            let boardHeader = document.createElement("div");
+            let boardTitle = document.createElement("span");
+            let addBtn = document.createElement("button");
+            let boardToggle = document.createElement("button");
+            let boardI = document.createElement("i");
+            let boardBackground = document.createElement("div");
+
+            boardContainer.setAttribute("class", "board-container");
+            boardSection.setAttribute("class", "board");
+            boardHeader.setAttribute("class", "board-header");
+            boardTitle.setAttribute("class", "board-title");
+            addBtn.setAttribute("class", "board-add");
+            boardToggle.setAttribute("class", "board-toggle");
+            boardI.setAttribute("class", "fas fa-chevron-down");
+            boardBackground.setAttribute("class", "board-columns");
+
+            boardTitle.innerText = board.title;
+            addBtn.innerText = "Add new board";
+            boardToggle.appendChild(boardI);
+            for (let child of [boardTitle, addBtn, boardToggle]) {
+                boardHeader.appendChild(child);
+            }
+
+            for (let status of boards.statuses) {
+                let container = document.createElement("div");
+                let columnTitle = document.createElement("div");
+                let columnContent = document.createElement("div");
+
+                container.setAttribute("class", "board-column");
+                columnTitle.setAttribute("class", "board-column-title");
+                columnContent.setAttribute("class", "board-column-content");
+                columnTitle.innerText = status.title;
+
+                for (let card of boards.cards) {
+
+                    if (status.id === card.status_id && board.id === card.board_id) {
+                        let cardBox = document.createElement("div");
+                        let cardRemove = document.createElement("div");
+                        let cardI = document.createElement("i");
+                        let cardTitle = document.createElement("div");
+
+                        cardBox.setAttribute("class", "card");
+                        cardRemove.setAttribute("class", "card-remove");
+                        cardI.setAttribute("class", "fas fa-trash-alt");
+                        cardTitle.setAttribute("class", "card-title");
+
+                        cardTitle.innerText = card.title;
+                        cardRemove.appendChild(cardI);
+                        cardBox.appendChild(cardRemove);
+                        cardBox.appendChild(cardTitle);
+                        columnContent.appendChild(cardBox);
+                    }
+                }
+                container.appendChild(columnTitle);
+                container.appendChild(columnContent);
+                boardBackground.appendChild(container);
+            }
+            boardSection.appendChild(boardHeader);
+            boardSection.appendChild(boardBackground);
+            boardContainer.appendChild(boardSection);
+        }
+        document.body.appendChild(boardContainer);
+    }
 };
